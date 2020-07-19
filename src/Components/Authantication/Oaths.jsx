1844ba7fa4053ba39d42117ad9e90c9e5a2e63d0
Pranav Agarwal <p2012agarwal@gmail.com>
2020-07-20 01:12:55 +0530

    bug fixes

--- src/Components/Authantication/Oaths.jsx
+++ src/Components/Authantication/Oaths.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
-const sg = () =>{}
-const lg = () =>{}
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
-const butn = (g) =>{
+const butn = () => {
     return (
         <div className='oth'>
-            <button onClick={g}><i className="fa fa-google"> <span className='authname'>Google</span></i></button>
+            <GoogleLogin
+                clientId="728214676777-493jan7d4lath88a9k15s40jq88bnj2o.apps.googleusercontent.com"
+                render={renderProps => (
+                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}><i className="fa fa-google"> <span className='authname'>Google</span></i></button>
+                )}
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+
         </div>
     );
 }
 
 export const SOaths = () => {
-   return butn(sg);
+    return butn();
 }
 
 export const LOaths = () => {
-    return butn(lg);
+    return butn();
 }
