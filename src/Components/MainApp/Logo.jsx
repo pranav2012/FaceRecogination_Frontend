735198e994152af0e3c55f046537e7876831e4c1
Pranav Agarwal <p2012agarwal@gmail.com>
2020-07-21 19:07:22 +0530

    mobile css fix

--- src/Components/MainApp/Logo.jsx
+++ src/Components/MainApp/Logo.jsx
@@ -4,9 +4,9 @@ import Tilt from 'react-tilt';
 
 function Logo() {
     return(
-        <div>
-            <Tilt className='Tilt br2 shadow-1' options={{max:25}} style={{height:150, width: 190, background:'linear-gradient(89deg,#FF5DEF 0%,#04C8DE 100%)'}}>
-                <img style={{height:130,width:170}} src={logo} alt='logo' />
+        <div className="logospace">
+            <Tilt className='Tilt br2 shadow-1' options={{max:25}} style={{height:150, width: 190, background:'transparent'}}>
+                <img classname='logoimg' style={{height:130,width:170}} src={logo} alt='logo' />
             </Tilt>
         </div>
     );
